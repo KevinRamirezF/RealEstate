3cdcd328b8c81e5a62ed312f23c639ac4040f5fc
KevinRamirezF <kevinramirezf@outlook.com>
2025-09-08 17:18:57 -0500

    feat: add pending files about Docker Compose and how the API is released

--- RealEstate.Application/DTOs/Input/LoginDto.cs
+++ RealEstate.Application/DTOs/Input/LoginDto.cs
@@ -8,12 +8,12 @@ public record LoginDto
     /// <summary>
     /// User's email address
     /// </summary>
-    /// <example>john.doe@example.com</example>
+    /// <example>admin@realestate.com</example>
     public string Email { get; init; } = string.Empty;
 
     /// <summary>
     /// User's password
     /// </summary>
-    /// <example>MySecurePassword123</example>
+    /// <example>Admin123!</example>
     public string Password { get; init; } = string.Empty;
 }
